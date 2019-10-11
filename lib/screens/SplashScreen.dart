16585dea7e7c006be33c3671f9788cbfdf7ce32f
Phúc Võ <phuc@MacBook-Air-cua-Phuc.local>
2019-10-11 14:10:47 +0700

    add splash screen

---
+++ lib/screens/SplashScreen.dart
@@ -0,0 +1,63 @@
+import 'dart:async';
+
+import 'package:fl_medec/screens/WelcomeScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 5),
+      () => Navigator.of(context).pushNamed(WelcomeScreen.routeName),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(110, 120, 247, 1),
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Medec',
+                        style: TextStyle(
+                          fontSize: 61.0,
+                          letterSpacing: 4.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      SpinKitThreeBounce(
+                        color: Colors.white,
+                        size: 16.0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/WelcomeScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  static const routeName = '/welcome';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('ok'),
+    );
+  }
+}
